e7524eabbfc56e1048ec02a68adab4fda8b3e975
Killian QUINTIN <killian.quintin@epitech.eu>
2025-02-11 14:16:03 +0100

    [FIX] strchr func can now find \0 char

--- src/strchr.asm
+++ src/strchr.asm
@@ -15,13 +15,16 @@ section .text
 
 .LOOP:
     mov r10b, [rax]         ; récupère le caractère sur lequel pointe rax
-    cmp r10b, 0             ; vérifie si le caractère récupéré est '\0'
-    je .NULL_RETURN
     cmp r10b, sil           ; vérifie si le caractère récupéré est égal à
                             ; 2nd_param
-    jne .INCREMENTER        ; si non, incrémente
-    ret                     ; si le jmp n'est pas appelé, c'est que le
-                            ; caractère est trouvé
+    je .RETURN              ; return rax (de type pointeur)
+    cmp r10b, 0             ; vérifie si le caractère récupéré est '\0'
+    je .NULL_RETURN
+    jmp .INCREMENTER        ; si non, incrémente et tourne une nouvelle fois
+                            ; dans la boucle
+
+.RETURN:
+    ret
 
 .NULL_RETURN:
     mov rax, 0              ; initialise la valeur de retour à NULL
