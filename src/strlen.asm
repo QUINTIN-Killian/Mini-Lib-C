06dde31ee30e77a75d4ac4eb5f38fbe5ec9c4f30
Killian QUINTIN <killian.quintin@epitech.eu>
2025-02-11 11:03:21 +0100

    [EDIT] strlen

--- src/strlen.asm
+++ src/strlen.asm
@@ -9,17 +9,17 @@ global strlen
 
 section .text
 
-incrementer:
+.INCREMENTER:
     inc rax                 ; incrémente l'itérateur (qui est aussi la return
                             ; value) de 1
 
-loop:
-    mov r10b, [rdi + rax]   ; r10 = 1st param [i (= rax)]
+.LOOP:
+    mov r10b, [rdi + rax]   ; r10b = 1st_param[i (= rax)]
     cmp r10b, 0             ; vérifie si on est à la fin de la string
-    jne incrementer         ; incrémente de 1
+    jne .INCREMENTER        ; incrémente de 1
     ret                     ; return le contenu de rax
 
 strlen:
     mov rax, 0              ; initialisation de la return value à 0
                             ; + sert d'itérateur
-    jmp loop                ; rentre dans la loop
+    jmp .LOOP               ; rentre dans la loop
